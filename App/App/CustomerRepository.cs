5f2eb56ef806735726d2dfb3dbc15c4591a061e7
nelson9 <nferguson@macmillan.org.uk>
2016-09-13 15:46:02 +0100

    initial commit

---
+++ App/App/CustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    public interface ICustomerRepository
+    {
+        void AddCustomer(Customer customer);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void AddCustomer(Customer customer)
+        {
+            CustomerDataAccess.AddCustomer(customer);
+        }
+    }
+}
